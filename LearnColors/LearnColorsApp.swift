4bf3a83df3ca3c2a33c7ba4cd947d86f226ddda8
Alex Karamanets <alexander.fromworld@gmail.com>
2022-12-29 16:35:46 +0100

    App to learn colors, added basic struct

---
+++ LearnColors/LearnColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnColorsApp.swift
+//  LearnColors
+//
+//  Created by Alex Karamanets on 29.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearnColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+}
---
+++ LearnColors/Main.swift
@@ -0,0 +1,24 @@
+//
+//  Main.swift
+//  LearnColors
+//
+//  Created by Alex Karamanets on 29.12.2022.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+   
+        ZStack {
+            
+        }
+    }
+}
+
+//                         🔱
+struct Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Main()
+    }
+}
