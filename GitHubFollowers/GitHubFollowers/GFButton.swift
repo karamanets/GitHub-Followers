f499bbed44e90ad18f46a23cd680da4ce1dde9b1
Alex Karamanets <alexander.fromworld@gmail.com>
2024-01-12 07:43:08 +0100

    Added Button and TextField

---
+++ GitHubFollowers/GitHubFollowers/GFButton.swift
@@ -0,0 +1,43 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Alex Karamanets on 11/01/2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    ///Custom init to make background an title
+    init(background: UIColor, title: String) {
+        super.init(frame: .zero) /// use AL
+        self.backgroundColor = background
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        layer.cornerRadius = 10
+        
+        layer.shadowColor = UIColor.systemGray4.cgColor
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.7
+        layer.shadowOffset = CGSize(width: 2, height: 2)
+    }
+    
+}
---
+++ GitHubFollowers/GitHubFollowers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Alex Karamanets on 11/01/2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true /// If long make it fit
+        minimumFontSize = 12             /// But no more then 12
+        
+        backgroundColor = .tertiarySystemBackground
+        
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+}
